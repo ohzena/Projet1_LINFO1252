b16cea3da5b8d7cd2c433fb4abffade2ce344da6
Sinon <simon.halliez@student.uclouvain.be>
2022-11-24 10:34:04 +0100

    Légère modif du experiments.sh

--- Projet1/Part1/experiments.sh
+++ Projet1/Part1/experiments.sh
@@ -5,10 +5,10 @@ for i in {2..64}
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
-        bin/./ProduceurConsumer ${i} 8192
-        End=$(date +%s.%N)
-        echo "${i}, ${j}, ${Start}, ${End}"
+        #Start=$(date +%s.%N)
+        /usr/bin/time -f "${i}, ${j}, %e" bin/./ProduceurConsumer ${i} 8192
+        #bin/./ProduceurConsumer ${i} 8192
+        #End=$(date +%s.%N)
     done
 done
 
@@ -18,10 +18,11 @@ for i in {1..64}
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
-        bin/./Philo ${i}
-        End=$(date +%s.%N)
-        echo "${i}, ${j}, ${Start}, ${End}"
+        #Start=$(date +%s.%N)
+        /usr/bin/time -f "${i}, ${j}, %e" bin/./Philo ${i}
+        #bin/./Philo ${i}
+        #End=$(date +%s.%N)
+        #echo "${i}, ${j}, ${Start}, ${End}"
     done
 done
 
@@ -31,9 +32,11 @@ for i in {2..64}
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
-        bin/./ReadWrite ${i}
-        End=$(date +%s.%N)
-        echo "${i}, ${j}, ${Start}, ${End}"
+        #Start=$(date +%s.%N)
+        #bin/./ReadWrite ${i}
+        /usr/bin/time -f "${i}, ${j}, %e" bin/./ReadWrite ${i}
+        #End=$(date +%s.%N)
+        #echo "${i}, ${j}, ${Start}, ${End}"
     done
 done
+make clean
