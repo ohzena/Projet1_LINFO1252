0aa19d0d10a305cf1b3001ed4a10a04a178891c2
Benzz <benita.tia@student.uclouvain.be>
2022-11-25 00:26:22 +0100

    encore quelque changement sur le bash

--- Part1/experiments.sh
+++ Part1/experiments.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 mkdir -p csv
 echo "Producer_Consumer"
-echo "N, J, Start, End" >> csv/pro_con.csv
+echo "Num, J, Start, End" > csv/pro_con.csv
 for i in {2..64} 
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
+        Start=$(date +%s.%Num)
         bin/./ProduceurConsumer ${i} 8192
-        End=$(date +%s.%N)
+        End=$(date +%s.%Num)
         echo "${i}, ${j}, ${Start}, ${End}" >> csv/pro_con.csv
     done
 done
 
 echo "Philosophes"
-echo "N, J, Start, End" >> philo.csv
+echo "Num, J, Start, End" > csv/philo.csv
 for i in {1..64} 
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
+        Start=$(date +%s.%Num)
         bin/./Philo ${i}
-        End=$(date +%s.%N)
+        End=$(date +%s.%Num)
         echo "${i}, ${j}, ${Start}, ${End}" >> csv/philo.csv
     done
 done
 
 echo "Reader_Whriter"
-echo "N, J, Start, End" >> csv/rw.csv
+echo "N, J, Start, End" > csv/rw.csv
 for i in {2..64} 
 do
     for j in {1..5} 
     do
-        Start=$(date +%s.%N)
+        Start=$(date +%s.%Num)
         bin/./ReadWrite ${i}
-        End=$(date +%s.%N)
+        End=$(date +%s.%Num)
         echo "${i}, ${j}, ${Start}, ${End}" >> csv/rw.csv
     done
 done
