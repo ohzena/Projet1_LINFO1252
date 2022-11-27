7752cff8f3ff0d77c6d4433b8215991e4a9212b2
Sinon <simon.halliez@student.uclouvain.be>
2022-11-27 18:54:20 +0100

    Redirection vers STDOUT lors des appelle de time

--- Projet1/Part1/experiments.sh
+++ Projet1/Part1/experiments.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
+
 echo "Producer_Consumer"
-echo "N, J, Start, End"
+echo "N,J,End"
 for i in {2..64} 
 do
     for j in {1..5} 
     do
-        #Start=$(date +%s.%N)
-        /usr/bin/time -f "${i}, ${j}, %e" bin/./ProduceurConsumer ${i} 8192
-        #bin/./ProduceurConsumer ${i} 8192
-        #End=$(date +%s.%N)
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ProduceurConsumer ${i} 8192
     done
 done
 
 echo "Philosophes"
-echo "N, J, Start, End"
+echo "N,J,End"
 for i in {1..64} 
 do
     for j in {1..5} 
     do
-        #Start=$(date +%s.%N)
-        /usr/bin/time -f "${i}, ${j}, %e" bin/./Philo ${i}
-        #bin/./Philo ${i}
-        #End=$(date +%s.%N)
-        #echo "${i}, ${j}, ${Start}, ${End}"
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./Philo ${i}
     done
 done
 
 echo "Reader_Whriter"
-echo "N, J, Start, End"
+echo "N,J,End"
 for i in {2..64} 
 do
     for j in {1..5} 
     do
-        #Start=$(date +%s.%N)
-        #bin/./ReadWrite ${i}
-        /usr/bin/time -f "${i}, ${j}, %e" bin/./ReadWrite ${i}
-        #End=$(date +%s.%N)
-        #echo "${i}, ${j}, ${Start}, ${End}"
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ReadWrite ${i}
     done
 done
 make clean
