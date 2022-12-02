afd76bccf4841a59d1d590d8b278efb92b92230c
Sinon <simon.halliez@student.uclouvain.be>
2022-12-02 17:26:07 +0100

    Ajout des sempahore sans commentaire et en sopiant collant les fct de mutex

---
+++ Projet1/Part2/code/semaphore.c
@@ -0,0 +1,155 @@
+#include <stdlib.h>   //Nbr ramdom
+#include <pthread.h>  //Mutex et thread
+#include <stdio.h> //EXIT_SUCCESS
+#include <string.h>
+#include <errno.h>
+#include <unistd.h> //sleep
+
+typedef struct my_mutex{
+    int *val;
+} my_mutex_t;
+
+
+
+int test_and_set(my_mutex_t mutex){
+    int y;
+    asm (
+            "movl $1, %%eax;"
+            "xchgl %%eax, (%1);"
+            "movl %%eax, %0;"
+            :"=&r"(y) /* y is output operand, note the  
+                & constraint modifier. */
+            :"r"(mutex.val)   /* ptr to val is input operand */
+            :"%eax"); /* %eax is clobbered register */
+    return y;
+}
+
+int my_mutex_lock_tts(my_mutex_t* mutex_ptr){
+    while (test_and_set(*mutex_ptr)) {
+        while (*(mutex_ptr-> val) != 0) {
+        }
+    }
+    return 0;
+}
+
+
+
+int my_mutex_init(my_mutex_t *mutex_ptr){
+    mutex_ptr->val = (int*)malloc(sizeof(int));
+    if (mutex_ptr->val == NULL){
+        return -1;
+    }
+    *(mutex_ptr->val) = 0;
+    return 0;
+}
+
+int my_mutex_destroy(my_mutex_t *mutex_ptr){
+    free(mutex_ptr->val);
+    return 0;
+}
+
+int my_mutex_unlock(my_mutex_t *mutex_ptr){
+
+    asm (
+        "movl $0, %%eax;"
+        "xchgl %%eax, (%0);"
+        :
+        :"r"(mutex_ptr->val)   /* ptr to val is input operand */
+        :"%eax"); /* %eax is clobbered register */
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Début de Semaphore.c
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct my_sem{
+    my_mutex_t * mutex_ptr;
+    int *val_ptr;
+} my_sem_t;
+
+my_sem_t sem;
+
+int my_sem_init(my_sem_t *sem_ptr,int init_val){
+    //Allocation and initiallisation the space for the value in the semaphore
+    sem_ptr->val_ptr = (int*)malloc(sizeof(int));
+    if(sem_ptr->val_ptr == NULL){
+        return -1;
+    }
+    *(sem_ptr->val_ptr)=init_val;
+
+    //Allocation and initiallisation the space for mutex
+    sem_ptr->mutex_ptr = (my_mutex_t*)malloc(sizeof(my_mutex_t));
+    if(sem_ptr->mutex_ptr == NULL){
+        return -1;
+    }
+    if(-1==my_mutex_init(sem_ptr->mutex_ptr)){
+        return -1;
+    }
+    return 0;
+}
+
+int my_sem_wait(my_sem_t *sem_ptr){
+    int go = 1;
+    while(go){
+        //Use the mutex to past the thread to ready state 
+        //and decrease the value (if value of semaphore > 0) 
+        //without concurrent modification.
+        my_mutex_lock_tts(sem_ptr->mutex_ptr);
+        if(*(sem_ptr->val_ptr)>0){
+            go = 0;
+            *(sem_ptr->val_ptr) -= 1;
+        }
+        my_mutex_unlock(sem_ptr->mutex_ptr);
+    }
+    return 0;
+}
+
+int my_sem_post(my_sem_t *sem_ptr){
+    my_mutex_lock_tts(sem_ptr->mutex_ptr);
+    *(sem_ptr->val_ptr) += 1;
+    my_mutex_unlock(sem_ptr->mutex_ptr);
+    return 0;
+}
+
+int my_sem_destroy(my_sem_t *sem_ptr){
+    my_mutex_destroy(sem_ptr->mutex_ptr);
+    free(sem_ptr->mutex_ptr);
+    free(sem_ptr->val_ptr);
+    return 0;
+}
+
+void *post(void * param) {
+    for(int i=0; i<2; i++){
+        printf("Benita dort\n");
+        sleep(3);
+        printf("Benita se reveille\n");
+        my_sem_post(&sem);
+    }
+    return;
+}
+
+int main (int argc, char *argv[])  {
+    int err;
+    my_sem_init(&sem,1);
+    printf("Val: %i\n",*(sem.val_ptr));
+    my_sem_wait(&sem);
+    printf("Val: %i\n",*(sem.val_ptr));
+    pthread_t thread;
+    err=pthread_create(&(thread),NULL,&post,NULL);
+    if(err!=0){
+        printf("Error pthread_create\n");
+        return -1;
+    }
+    printf("SImon regarde Benita\n");
+    my_sem_wait(&sem);
+    printf("SImon regarde le cours\n");
+    my_sem_wait(&sem);
+    err=pthread_join(thread,NULL);
+    if(err!=0){
+        printf("Error pthread_join\n");
+        return -1;
+    }
+    my_sem_destroy(&sem);
+    printf("Le cours est fini\n");
+    return 0;
+}
