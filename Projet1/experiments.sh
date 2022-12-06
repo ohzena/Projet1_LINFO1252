b1a8c4c8ac012596220768cba4cf3579a06b294e
Sinon <simon.halliez@student.uclouvain.be>
2022-12-06 14:50:23 +0100

    Ajout des fct disparue et mise à la bonne valeur de produceur consummer

--- Projet1/experiments.sh
+++ Projet1/experiments.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+N_MAX_THREAD=8
+
 echo "Producer_Consumer"
 echo "N,J,End"
-for ((i=2; i <= 64; i=i*2))
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
-        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ProduceurConsumer ${i} 64192
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ProduceurConsumer ${i} 8192
     done
 done
 
 echo "Philosophes"
 echo "N,J,End"
-for ((i=1; i <= 64; i=i*2))
+for ((i=1; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
@@ -22,7 +24,7 @@ done
 
 echo "Reader_Whriter"
 echo "N,J,End"
-for ((i=2; i <= 64; i=i*2))
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
@@ -33,7 +35,7 @@ done
 
 echo "Test_And_Set"
 echo "N,J,End"
-for ((i=1; i <= 64; i=i*2))
+for ((i=1; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
@@ -44,7 +46,7 @@ done
 
 echo "Test_And_Test_And_Set"
 echo "N,J,End"
-for ((i=1; i <= 64; i=i*2))
+for ((i=1; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
@@ -54,22 +56,63 @@ done
 
 echo "My_Producer_Consumer"
 echo "N,J,End"
-for ((i=2; i <= 64; i=i*2))
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
-        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./MyProduceurConsumer ${i} 64192
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./MyProduceurConsumer ${i} 8192
     done
 done
 
 echo "My_Philosophes"
 echo "N,J,End"
-for ((i=1; i <= 64; i=i*2))
+for ((i=1; i <= $N_MAX_THREAD; i=i*2))
 do
     for j in {1..5} 
     do
         /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./MyPhilo ${i}
     done
 done
+
+echo "My_Reader_Whriter"
+echo "N,J,End"
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
+do
+    for j in {1..5} 
+    do
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./MyReadWrite ${i}
+    done
+done 
+
+
+echo "Producer_Consumer_TTAS"
+echo "N,J,End"
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
+do
+    for j in {1..5} 
+    do
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ProduceurConsumerTTAS ${i} 8192
+    done
+done
+
+echo "Philosophes_TTAS"
+echo "N,J,End"
+for ((i=1; i <= $N_MAX_THREAD; i=i*2))
+do
+    for j in {1..5} 
+    do
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./PhiloTTAS ${i}
+    done
+done
+
+echo "Reader_Whriter_TTAS"
+echo "N,J,End"
+for ((i=2; i <= $N_MAX_THREAD; i=i*2))
+do
+    for j in {1..5} 
+    do
+        /usr/bin/time -o /dev/stdout -f "${i}, ${j}, %e" bin/./ReadWriteTTAS ${i}
+    done
+done 
 
 make clean
